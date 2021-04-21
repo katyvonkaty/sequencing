ce70c3867c08171e4e532ac757100256b7283489
katie <kj0203@gmail.com>
2021-04-21 09:29:36 -0400

    updated

--- src/richest-people.js
+++ src/richest-people.js
@@ -13,30 +13,112 @@ import tree from "./assets/images/tree.png";
 import halfmoon from "./assets/images/halfmoon.png";
 
 
+const richestPeople = [
+  {
+    pose: "plank",
+    imgUrl: `${plank}`,
+    type: "puttering",
+    english: "plank",
+    cues: "grip the floor"
+  },
+  {
+    pose: "utanasana",
+    imgUrl: `${utanasana}`,
+    type: "puttering",
+    english: "childs pose",
+  },
+  {
+    pose: "bhjuangasana",
+    imgUrl: `${bhjungasana}`,
+    type: "puttering",
+    english: "cobra",
+  },
+  {
+    pose: "lowlunge",
+    imgUrl: `${lowlunge}`,
+    type: "puttering",
+    english: "low lunge",
+  },
+  {
+    pose: "balasana",
+    imgUrl: `${balasana}`,
+    type: "puttering",
+    english: "childs pose",
+  },
+  { pose: "caturanga", imgUrl: `${caturanga}`, type: "puttering", english: "" },
+  { pose: "cobra", imgUrl: `${cobra}`, type: "puttering", english: "plank" },
+  {
+    pose: "downward dog",
+    imgUrl: `${downward}`,
+    type: "puttering",
+    english: "downward dog",
+  },
 
-export default [
-  { pose: "plank",  imgUrl: `${plank}`, type:"puttering", english:"plank", cues:"grip the floor", cues:""},
-  { pose: "utanasana", imgUrl: `${utanasana}`, type:"puttering", english:"childs pose" },
-  { pose: "bhjuangasana", imgUrl: `${bhjungasana}`, type:"puttering", english:"cobra" },
-  { pose: "lowlunge", imgUrl: `${lowlunge}` , type:"puttering", english:"low lunge"},
-  { pose: "balasana",  imgUrl: `${balasana}`, type:"puttering", english:"childs pose"},
-  { pose: "caturanga", imgUrl: `${caturanga}`, type:"puttering", english:"" },
-  { pose: "cobra",  imgUrl: `${cobra}`, type:"puttering", english:"plank"},
-  { pose: "downward dog", imgUrl: `${downward}`, type:"puttering", english:"downward dog" },
+  {
+    pose: "Virabhadrasana 2",
+    imgUrl: `${warrior2}`,
+    type: "opening standing",
+    english: "warrior 2",
+  },
+  {
+    pose: "Utthita Trikonasana",
+    imgUrl: `${triangle}`,
+    type: "opening standing",
+    english: "triangle",
+    cues:
+      "Twist from the navel, Lengthen the crown of your head towards the front of the room, Keep the lower ribs connected to the body",
+  },
+  {
+    pose: "Utthita Parsvakonasana",
+    imgUrl: `${extended}`,
+    type: "opening standing",
+    english: "extended side angle",
+    cues:
+      "Side elongates over the side of your thigh,Pull your outer hip underneath you, Twist from the navel",
+  },
+  {
+    pose: "Ardha Chandrasana",
+    imgUrl: `${tree}`,
+    type: "opening standing",
+    english: "Half Moon Pose",
+  },
+  {
+    pose: "Vrkasasana",
+    imgUrl: `${halfmoon}`,
+    type: "opening standing",
+    english: "Tree Pose",
+  },
 
-  { pose: "Virabhadrasana 2", imgUrl: `${warrior2}`, type:"opening standing", english:"warrior 2" },
-  { pose: "Utthita Trikonasana", imgUrl: `${triangle}` , type:"opening standing", english:"triangle", cues:
-  "Twist from the navel, Lengthen the crown of your head towards the front of the room, Keep the lower ribs connected to the body"},
-  { pose: "Utthita Parsvakonasana",  imgUrl: `${extended}`, type:"opening standing", english:"extended side angle", cues:"Side elongates over the side of your thigh,Pull your outer hip underneath you, Twist from the navel"},
-  { pose: "Ardha Chandrasana", imgUrl: `${tree}`, type:"opening standing", english:"Half Moon Pose"},
-  { pose: "Vrkasasana", imgUrl: `${halfmoon}`, type:"opening standing", english:"Tree Pose"},
-
-  { pose: "Virabhadrasana 2", imgUrl: `${warrior2}`, type:"opening standing", english:"warrior 2" },
-  { pose: "Utthita Trikonasana", imgUrl: `${triangle}` , type:"opening standing", english:"triangle"},
-  { pose: "Utthita Parsvakonasana",  imgUrl: `${extended}`, type:"opening standing", english:"extended side angle"},
-  { pose: "Ardha Chandrasana", imgUrl: `${tree}`, type:"opening standing", english:"Half Moon Pose"},
-  { pose: "Vrkasasana", imgUrl: `${halfmoon}`, type:"opening standing", english:"Tree Pose"}
-
-
-
+  {
+    pose: "Virabhadrasana 2",
+    imgUrl: `${warrior2}`,
+    type: "opening standing",
+    english: "warrior 2",
+  },
+  {
+    pose: "Utthita Trikonasana",
+    imgUrl: `${triangle}`,
+    type: "opening standing",
+    english: "triangle",
+  },
+  {
+    pose: "Utthita Parsvakonasana",
+    imgUrl: `${extended}`,
+    type: "opening standing",
+    english: "extended side angle",
+  },
+  {
+    pose: "Ardha Chandrasana",
+    imgUrl: `${tree}`,
+    type: "opening standing",
+    english: "Half Moon Pose",
+  },
+  {
+    pose: "Vrkasasana",
+    imgUrl: `${halfmoon}`,
+    type: "opening standing",
+    english: "Tree Pose",
+  },
 ];
+
+export default richestPeople
